296ef7611c17c285b889d59bb47ee074ec637a48
Arthur Ferreira <arthur13042001@gmail.com>
2023-01-19 16:01:58 -0300

    feat: end portfólio

---
+++ src/utils/stackData.ts
@@ -0,0 +1,43 @@
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiPostgresql, SiTypescript } from "react-icons/si";
+import { FaGit, FaPython, FaVuejs } from "react-icons/fa";
+import {
+  FaDocker,
+  FaHtml5,
+  FaCss3Alt,
+  FaJs,
+  FaNode,
+  FaReact,
+} from "react-icons/fa";
+
+export const stackData = [
+  {
+    title: "HTML",
+    img: FaHtml5,
+  },
+  {
+    title: "CSS",
+    img: FaCss3Alt,
+  },
+  {
+    title: "JS",
+    img: FaJs,
+  },
+  {
+    title: "Vue",
+    img: FaVuejs
+  },
+  {
+    title: "Node JS",
+    img: FaNode,
+  },
+  {
+    title: "React",
+    img: FaReact,
+  },
+  { title: "Next.js", img: TbBrandNextjs },
+  { title: "Docker", img: FaDocker },
+  { title: "PostgreSQL", img: SiPostgresql },
+  { title: "Git", img: FaGit },
+  { title: "Python", img: FaPython },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Arthur Ferreira",
+  githubUser: "arturfer",
+  linkedinUser: "arthur-ferreira1",
+  whatsappNumber: "+5535984528234",
+  emailUser: "arturfer1315@gmail.com",
+  instagramUser: "",
+  facebookUser: "",
+};
